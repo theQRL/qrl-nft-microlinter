b1de069dd7e05dbefc4a5e367ece4c3d938dc1fa
JP Lomas <jp@theqrl.org>
2022-03-26 14:10:54 +0000

    Remove trailing slash in production URL

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 let host = 'http://localhost:3000';
 if (process.env.NODE_ENV !== 'production') {
-  host = 'https://nft-linter.theqrl.org/';
+  host = 'https://nft-linter.theqrl.org';
 }
 
 app.use(
